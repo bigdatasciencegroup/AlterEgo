6d031fe927d0273b92bda57d6fdc4e46a35a4516
Utkarsh Sarawgi <usarawgi911@gmail.com>
2019-07-26 19:58:07 -0400

    code cleaned up

--- phoneme-ml-models/decode_and_error_analysis.py
+++ phoneme-ml-models/decode_and_error_analysis.py
@@ -445,6 +445,79 @@ def plot_confusion_matrix(y_true, y_pred, classes, normalize=False, title=None, 
     fig.tight_layout()
     return ax
 
+def plot_confusion_matrix(sequences, labels, classes, decode='batch_greedy_decode', normalize=False, title=None, cmap=plt.cm.Blues):
+    """
+    This function plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+
+    actual_labels = list(np.argmax(labels, axis=-1))
+    if decode=='batch_greedy_decode':       predicted_labels = batch_greedy_decode(sequences)
+
+    counter = 0 # Tracks number of predictions with same length as actual target
+    y_true, y_pred = [], []
+
+    for index in range(len(sequences)):
+        if (actual_labels[index].shape == predicted_labels[index].shape):
+            for x in list(actual_labels[index]):
+                if x!=start_symbol and x!=end_symbol:       y_true.append(x)
+            for x in list(predicted_labels[index]):
+                if x!=start_symbol and x!=end_symbol:       y_pred.append(x)
+            counter+=1
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+
+    print('Sequences considered for confusion matrix : {} out of {}'.format(counter, len(sequences)))
+    np.set_printoptions(precision=2)
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    classes = classes[unique_labels(y_true, y_pred)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Plotting Normalized confusion matrix")
+    else:
+        print("Plotting Confusion matrix, without normalization")
+
+    # print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
+
+
+# def accuracies(sequences,labels):
+
+
 
 # --------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -460,42 +533,18 @@ class_names = ['AA', 'AE', 'AH', 'AO', 'AW', 'AY', 'EH', 'ER', 'EY', 'IH', 'IY',
 model = load_model('SavedModels/Full_{}.h5'.format(log_name))
 encoder_model = load_model('SavedModels/Encoder_{}.h5'.format(log_name))
 decoder_model = load_model('SavedModels/Decoder_{}.h5'.format(log_name))
-# print(model.metrics_tensors)
 
-actual_labels = list(np.argmax(labels, axis=-1))
-print '\nActual labels :', len(actual_labels)
-# print actual_labels
-
+# actual_labels = list(np.argmax(labels, axis=-1))
 # predicted_labels = beam_decode(sequences)
 # predicted_labels = greedy_decode(sequences)
-predicted_labels = batch_greedy_decode(sequences)
-print 'Predicted labels :', len(predicted_labels)
-# print predicted_labels
+# predicted_labels = batch_greedy_decode(sequences)
 
 
-counter = 0 # Tracks number of predictions with same length as actual target
-y_true, y_pred = [], []
-
-for index in range(len(sequences)):
-    if (actual_labels[index].shape == predicted_labels[index].shape):
-        for x in list(actual_labels[index]):
-            if x!=start_symbol and x!=end_symbol:       y_true.append(x)
-        for x in list(predicted_labels[index]):
-            if x!=start_symbol and x!=end_symbol:       y_pred.append(x)
-        counter+=1
-y_true = np.array(y_true)
-y_pred = np.array(y_pred)
-
-print('Sequences considered for confusion matrix : {} out of {}'.format(counter, len(sequences)))
-
-np.set_printoptions(precision=2)
-
-# Plot non-normalized confusion matrix
-plot_confusion_matrix(y_true, y_pred, classes=np.array(class_names),
+plot_confusion_matrix(sequences, labels, np.array(class_names),
                       title='Confusion matrix, without normalization')
 
 # Plot normalized confusion matrix
-plot_confusion_matrix(y_true, y_pred, classes=np.array(class_names), normalize=True,
+plot_confusion_matrix(sequences, labels, np.array(class_names), normalize=True,
                       title='Normalized confusion matrix')
 
 plt.show()
