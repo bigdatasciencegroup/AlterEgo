38acc535752727531f8cdbd4560c895ef4ea60e5
Utkarsh Sarawgi <usarawgi911@gmail.com>
2019-08-24 16:03:42 -0400

    latest changes boston-home and model summary

---
+++ boston-home/model_summary.py
@@ -0,0 +1,61 @@
+import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
+os.environ['TF_CPP_MIN_LOG_LEVEL'] ='2' # TF INFO and WARNING messages are not printed
+
+import tensorflow as tf
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.8)
+
+from tensorflow.python.util import deprecation
+deprecation._PRINT_DEPRECATION_WARNINGS = False # Disables printing deprecation warnings
+
+import numpy as np
+from sklearn.utils.class_weight import compute_class_weight
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+from display_utils import DynamicConsoleTable
+import math
+import time
+import os.path
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
+
+
+learning_rate = 1e-4
+dropout_rate = 0.4
+num_classes = 15
+length = 900
+channels = range(8)
+
+inputs = tf.placeholder(tf.float32,[None, length, len(channels)]) #[batch_size,timestep,features]
+targets = tf.placeholder(tf.int32, [None, num_classes])
+weights = tf.placeholder(tf.float32, [None])
+training = tf.placeholder(tf.bool)
+
+conv1 = tf.layers.conv1d(inputs, 400, 12, activation=tf.nn.relu, padding='valid')
+conv1 = tf.layers.max_pooling1d(conv1, 2, strides=2)
+conv2 = tf.layers.conv1d(conv1, 400, 6, activation=tf.nn.relu, padding='valid')
+conv2 = tf.layers.max_pooling1d(conv2, 2, strides=2)
+conv3 = tf.layers.conv1d(conv2, 400, 3, activation=tf.nn.relu, padding='valid')
+conv3 = tf.layers.max_pooling1d(conv3, 2, strides=2)
+conv4 = tf.layers.conv1d(conv3, 400, 3, activation=tf.nn.relu, padding='valid')
+conv4 = tf.layers.max_pooling1d(conv4, 2, strides=2)
+conv5 = tf.layers.conv1d(conv4, 400, 3, activation=tf.nn.relu, padding='valid')
+conv5 = tf.layers.max_pooling1d(conv5, 2, strides=2)
+dropout = tf.layers.dropout(conv5, dropout_rate, training=training)
+reshaped = tf.reshape(dropout, [-1, np.prod(dropout.shape[1:])])
+fc1 = tf.layers.dense(reshaped, 250, activation=tf.nn.relu)
+logits = tf.layers.dense(fc1, num_classes, activation=tf.nn.softmax)
+
+loss = tf.reduce_mean(tf.multiply(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=targets), weights))
+
+optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+
+correct = tf.equal(tf.argmax(logits,1), tf.argmax(targets,1))
+accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+    writer = tf.summary.FileWriter("output", sess.graph)
+    # print sess.run(logits)
+    writer.close()
